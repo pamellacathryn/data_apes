4628d68f8776ac5f7c3c003cdc5f4dc1c8a01f2c
Pamella Cathryn <46389899+pamellacathryn@users.noreply.github.com>
2022-11-20 02:38:10 +0700

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,24 +1,111 @@
 import streamlit as st
 import altair as alt
-import plotly.express as px
-import yfinance as yf
-from pandas_datareader import data
-import datetime
 from streamlit_option_menu import option_menu
-from warnings import simplefilter
-import pandas as pd
 from PIL import Image
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
-from sklearn.metrics import mean_squared_error
-from scipy.signal import periodogram
-from pandas_datareader.data import DataReader
 import yfinance as yf
 from datetime import datetime, timedelta
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pickle
 
-stopper = False
+def find_sector_emiten(emiten):
+    sector = data_sector.loc[data_sector[0] == emiten, 2].values[0]
+    return sector
+
+
+def predict(index, emiten, orde, dataset):
+    dataset1 = dataset.loc[dataset['Emiten'] == emiten, ['Peningkatan', col_names[index]]]
+    dataset1.columns = [1, 2]
+    [x, y] = [dataset1.loc[:, 1], dataset1.loc[:, 2]]
+    mymodel = np.poly1d(np.polyfit(x, y, orde))
+    test = mymodel(2022)
+    score = r2_score(y, mymodel(x))
+
+    return [test, score]
+
+
+def execute(sector, dataset):
+    test_pred = pd.DataFrame({col: [] for col in col_names[:-1]})
+    test_acc = pd.DataFrame({col: [] for col in col_names[:-1]})
+    orde = 3
+
+    for i in emiten_sector.loc[emiten_sector[2] == sector, 0]:
+        pred_list = [i, sector, 2022]
+        acc_list = [i, sector, 2022]
+        try:
+            for j in range(3, len(col_names) - 1):
+                test = predict(j, i, orde, dataset)
+                pred_list.append(test[0])
+                acc_list.append(test[1])
+
+            df_app1 = pd.DataFrame({c: [v] for c, v in zip(col_names[:-1], pred_list)})
+            test_pred = pd.concat([test_pred, df_app1])
+            df_app2 = pd.DataFrame({c: [v] for c, v in zip(col_names[:-1], acc_list)})
+            test_acc = pd.concat([test_acc, df_app2])
+
+        except:
+            pass
+
+    return (test_pred, test_acc)
+
+
+def gen_sector(sector):
+    X = datazz.iloc[:, 3:11].values
+    Y = datazz.iloc[:, 11].values
+
+    X_train, _, _, _ = train_test_split(X, Y, test_size=0.2, random_state=20000)
+
+    sc = StandardScaler()
+    sc.fit(X_train)
+
+    col_sc = col_names[3:11]
+
+    arr_sc = sc.transform(datazz.iloc[:, 3:11])
+    arr_sc_ = np.array([arr_sc[:, i] for i in range(8)])
+
+    data_sc = pd.DataFrame({k: v for k, v in zip(col_sc, arr_sc_)})
+    data_sc1 = datazz.iloc[:, 0:3]
+
+    perc_sc = datazz.iloc[:, -1]
+
+    datasetgen = pd.concat([data_sc1, data_sc, perc_sc], axis=1)
+
+    data_pred, data_acc = execute(sector, datasetgen)
+
+    return (data_pred, datasetgen)
+
+
+def fundamental_rekom(sector):
+    data_pred, datasetgen = gen_sector(sector)
+    pickled_model = pickle.load(open(f'Regressor {sector}.pkl', 'rb'))
+
+    inc_pred = pd.DataFrame({'Emiten': [], 'Percentage Increase (%)': []})
+    emit = data_pred['Emiten'].unique()
+
+    rasio_cols = data_pred.columns[3:]
+
+    for e in emit:
+        arr_pred = data_pred.loc[data_pred['Emiten'] == e, rasio_cols].values.reshape(1, -1)
+        preds = pickled_model.predict(arr_pred)
+
+        pred_app = pd.DataFrame({'Emiten': [e], 'Percentage Increase (%)': preds[0]})
+        inc_pred = pd.concat([inc_pred, pred_app])
+
+    inc_pred = inc_pred.sort_values('Percentage Increase (%)', ascending=False)
+
+    X_test_ = datasetgen.iloc[:, 3:11]
+    Y_test_ = datasetgen.iloc[:, 11]
+    Y_pred_ = pickled_model.predict(X_test_)
+
+    skor = np.abs(mean_squared_error(Y_pred_, Y_test_)) ** 0.5
+
+    return (inc_pred, skor)
 
 def get_chart(data):
     data["Close"] = round(data["Close"], 2)
@@ -54,9 +141,9 @@ def get_chart(data):
     return (lines + points + tooltips).interactive()
 
 
+
 def create_models(fourier_pairs, target, kode_emiten):
     fourier = CalendarFourier(freq="A", order=fourier_pairs)
-
     dp = DeterministicProcess(
         index=datumz.index,
         constant=True,
@@ -65,20 +152,14 @@ def create_models(fourier_pairs, target, kode_emiten):
         additional_terms=[fourier],
         drop=True,
     )
-
     X = dp.in_sample()
-
     linreg_model = LinearRegression(fit_intercept=False)
     linreg_model.fit(X, target)
-
     target_pred = pd.Series(linreg_model.predict(X), index=target.index)
-
     features_fore = dp.out_of_sample(steps=1)
     target_fore = pd.Series(linreg_model.predict(features_fore), index=features_fore.index)
     rmse = mean_squared_error(target, target_pred, squared=False)
-
     perc = (target_fore[0] * 100 / target[-1]) - 100
-
     return (target, target_pred, target_fore, perc)
 
 
@@ -92,7 +173,6 @@ def create_plot(target, target_pred, target_fore, ax, kode):
     plt.show()
     return
 
-
 def check_pred(target, target_fore, perc):
     if target[-1] > 5000:
         cap = 20
@@ -100,19 +180,16 @@ def check_pred(target, target_fore, perc):
         cap = 25
     else:
         cap = 35
-
     if np.abs(perc) > cap and perc > 0:
         perc = cap
         target_fore = target[-1] * (100 + cap)
     elif np.abs(perc) > cap and perc > 0:
         perc = cap * -1
         target_fore = target[-1] * (100 + cap)
-
     return (target_fore, perc)
 
 def create_and_plot_models(fourier_pairs, target, ax, title, y_label):
     fourier = CalendarFourier(freq="A", order=fourier_pairs)
-
     dp = DeterministicProcess(
         index=datas.index,
         constant=True,
@@ -121,14 +198,10 @@ def create_and_plot_models(fourier_pairs, target, ax, title, y_label):
         additional_terms=[fourier],
         drop=True,
     )
-
     X = dp.in_sample()
-
     linreg_model = LinearRegression(fit_intercept=False)
     linreg_model.fit(X, target)
-
     target_pred = pd.Series(linreg_model.predict(X), index=target.index)
-
     features_fore = dp.out_of_sample(steps=1)
     target_fore = pd.Series(linreg_model.predict(features_fore), index=features_fore.index)
     rmse = mean_squared_error(target, target_pred, squared=False)
@@ -140,34 +213,23 @@ def create_and_plot_models(fourier_pairs, target, ax, title, y_label):
         ax.set_title(f'{title}\nPredictions RMSE: {rmse}')
         ax.set_ylabel(y_label)
         ax.legend(['Price', 'Model Predictions', 'Model Forecasts'])
-
     return target_fore
 
 
 def calc_perc_up_down(target, target_fore):
     last_data = target.values[-1]
     last_date = datas.index[-1]
-
     dates = [last_date + timedelta(days=i) for i in range(1, 2)]
     perc = []
-
     for i in range(0, len(target_fore)):
         calc = (target_fore[i] * 100 / last_data) - 100
         perc.append(calc)
-
     percentage = pd.DataFrame({'Date': dates, 'Percentage (%)': perc})
-
     return percentage
 
-def execute(datas, company_code, ax):
-
-  target_fore = create_and_plot_models(12, datas['Close'], ax, 'TCS High Stock Price - Seasonal Forecast', 'High Price')
-
-  percentage_data = calc_perc_up_down(datas['Close'], target_fore)
-
-  return (datas['Close'].values[-1], target_fore[0], percentage_data['Percentage (%)'].values[0])
-
 df = pd.read_table("Saham per Sektor(3).csv",delimiter=",")
+data_sector = pd.read_csv("Saham per Sektor Tanpa JK.csv", header=None, delimiter=",", skiprows=1)
+emiten_sector = data_sector.iloc[:, [0,2]]
 
 st.set_page_config(
     page_title="Saham Web App",
@@ -196,6 +258,8 @@ if select == "Main":
     st.markdown("<h6 style='text-align: center; '>by Data Apes</h6>",
                 unsafe_allow_html=True)
     st.write("")
+    image = Image.open('stock.jpg')
+
     st.header('Start Your Analysis and Create Predictions!')
     koloms1, koloms2, koloms3 = st.columns([1,1,1])
     profil = koloms1.radio("Your Investment Type",('Short Term', 'Long Term'))
@@ -233,8 +297,35 @@ if select == "Main":
                 kol2.write(f"Website: {symbol.get('website')}")
                 kol2.markdown(f"<div style='text-align: justify;'>{symbol.get('longBusinessSummary')}</div>", unsafe_allow_html=True)
                 st.write("")
+                if profil == "Long Term":
+                    datazz = pd.read_csv(f"{sector}.csv", delimiter=",").drop('Unnamed: 0', axis=1)
+                    col_names = datazz.columns
+                    sector = find_sector_emiten(emiten)
+                    top_10_rekomendasi, skor = fundamental_rekom(sector)
+                    tablezz = top_10_rekomendasi.loc[top_10_rekomendasi["Emiten"] == emiten,:]
+                    hide_table_row_index = """
+                                <style>
+                                thead tr th:first-child {display:none}
+                                tbody th {display:none}
+                                </style>
+                                """
+                    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+                    st.table(tablezz)
+                    meannn = tablezz["Percentage Increase (%)"].iloc[0]
+                    st.warning(f'The Percentage Increase of {tablezz["Emiten"].iloc[0]} may vary between {round(meannn-skor, 2)}% and {round(meannn+skor, 2)}%', icon="💡")
+                    st.write("")
+                    rasio = st.selectbox(
+                            'Choose ratio:',
+                            ('PB', 'ROA', 'ROE', 'EPS', 'PER', 'DER', 'DAR', 'Cash Flow', 'Percentage Increase (%)'))
+                    st.markdown(f"<h4 style='text-align: center; '>Visualization of the {rasio} of {emiten} to the Average {rasio} of the {sector} Sector</h4>", unsafe_allow_html=True)
+                    yearly_sektor_mean = datazz.groupby('Peningkatan').mean()[rasio]
+                    yearly_emiten = datazz.loc[datazz['Emiten'] == emiten, :].groupby('Peningkatan').mean()[rasio]
+                    gabungin = pd.concat([yearly_sektor_mean, yearly_emiten], axis=1)
+                    gabungin.columns = [rasio+"_Sector",rasio]
+                    st.line_chart(gabungin)
 
-                if profil == "Short Term":
+
+                elif profil == "Short Term":
                     st.markdown(f"<h4 style='text-align: center; '>Stock Price Forecasting of {emiten}</h4>",
                                 unsafe_allow_html=True)
                     col1, col2 = st.columns([3,1])
@@ -350,6 +441,36 @@ if select == "Main":
                     tampilkan.append(i)
                 tampil_chart = gabung[tampilkan]
                 colz1.line_chart(tampil_chart)
+
+            elif profil == "Long Term":
+                datazz = pd.read_csv(f"{sector}.csv", delimiter=",").drop('Unnamed: 0', axis=1)
+                col_names = datazz.columns
+                top_10_rekomendasi, skor = fundamental_rekom(sector)
+                st.markdown(f"<h4 style='text-align: center; '>Top 10 Stock's Growth Percentage on {sector}</h4>",
+                            unsafe_allow_html=True)
+                top_10_rekomendasi = top_10_rekomendasi[0:10]
+                top_10_rekomendasi.index = [i for i in range(1,11)]
+                st.table(top_10_rekomendasi)
+                st.warning(f'The Percentage Increase may vary between ±{round(skor,2)}% of the value shown above', icon="💡")
+                st.write("")
+                kyolumn1, kyolumn2 = st.columns([1,1])
+                list_em = []
+                for i in top_10_rekomendasi["Emiten"].to_list():
+                    list_em.append(i)
+                emiten = kyolumn1.selectbox(
+                    'Choose emiten:',
+                    (list_em))
+                rasio = kyolumn2.selectbox(
+                    'Choose ratio:',
+                    ('PB', 'ROA', 'ROE', 'EPS', 'PER', 'DER', 'DAR', 'Cash Flow', 'Percentage Increase (%)'))
+                st.markdown(
+                    f"<h4 style='text-align: center; '>Visualization of the {rasio} of {emiten} to the Average {rasio} of the {sector} Sector</h4>",
+                    unsafe_allow_html=True)
+                yearly_sektor_mean = datazz.groupby('Peningkatan').mean()[rasio]
+                yearly_emiten = datazz.loc[datazz['Emiten'] == emiten, :].groupby('Peningkatan').mean()[rasio]
+                gabungin = pd.concat([yearly_sektor_mean, yearly_emiten], axis=1)
+                gabungin.columns = [rasio + "_Sector", rasio]
+                st.line_chart(gabungin)
 
 elif select == "Stock List":
     st.header("Indonesia Stock Indexes")
