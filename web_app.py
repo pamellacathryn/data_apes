f5a06f0b483435d261e7b4f9199ab3b210559f91
Pamella Cathryn <46389899+pamellacathryn@users.noreply.github.com>
2022-11-21 13:06:53 +0700

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -330,7 +330,7 @@ if select == "Main":
                     col1, col2 = st.columns([3,1])
                     start = col2.date_input(
                         "Start Date:",
-                        datetime(2022, 11, 1))
+                        datetime(2022, 11, 14))
 
                     datumz = yf.download(emiten_jk, start)
                     _, ax = plt.subplots()
