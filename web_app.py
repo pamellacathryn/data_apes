fa2502c8a2f8cf21fa828cd94af74e40720f705e
Pamella Cathryn <46389899+pamellacathryn@users.noreply.github.com>
2022-11-22 12:56:05 +0700

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -398,8 +398,8 @@ if select == "Main":
                             pass
                     result = result.sort_values('Peningkatan (%)', ascending=False)
                     result = result[0:10]
-                    result = result.set_index([pd.Index([i for i in range(1,len(result)+1)])])
-                    st.markdown(f"<h4 style='text-align: center; '>Top {len(result)} Stock's Growth Percentage on {sector}</h4>",
+                    result.index = np.arange(1, len(result) + 1)
+                    st.markdown(f"<h4 style='text-align: center; '>Top 10 Stock's Growth Percentage on {sector}</h4>",
                                 unsafe_allow_html=True)
                     for i in range(10):
                         result = result.replace([result.iloc[i]["Kode Perusahaan"]], result.iloc[i]["Kode Perusahaan"].replace('.JK', ''))
